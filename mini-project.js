1bd2a89d8aec92f53cd4a764f17baf5c7e0fd3aa
SEIDY-KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2023-06-05 12:41:10 +0300

    reading attitude file's content

--- mini-project.js
+++ mini-project.js
@@ -9,3 +9,14 @@ const analysisNumber = document.querySelector(".number");
 const attitudesContent = document.querySelector(".attitudes");
 const resultContent = document.querySelector(".resultContent");
 const attitudes = attitudesContent.textContent.split("@@");
+
+//Reading from attitude.txt file and put file's content to attitude class (in index.html)
+const path="attitude.txt";
+const readTextFile = async function (path) {
+  const resp = await fetch(path);
+  const data=await resp.text();
+  attitudesContent.innerHTML=data.toString();
+  //console.log(attitudesContent.textContent);
+
+};
+readTextFile(path);
